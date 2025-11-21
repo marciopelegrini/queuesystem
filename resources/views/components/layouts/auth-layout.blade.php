5cd48a316254b0d8334cd55569efdc0be405ca0b
Marcio Pelegrini <marcio.pelegrini@gmail.com>
2025-11-21 15:53:55 -0500

    login_logout

--- resources/views/components/layouts/auth-layout.blade.php
+++ resources/views/components/layouts/auth-layout.blade.php
@@ -1,22 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
 <head>
-<title>{{ config('app.name') }} {!! empty($subtitle) ? '' : ' &vellip; ' . $subtitle !!}</title>
-<link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/png">
-{{-- Load resources --}}
-<link rel="stylesheet" href="{{ asset('assets/fontawesome/css/all.min.css') }}">
-@vite('resources/css/app.css')
+    <title>{{ config('app.name') }} {!! empty($subtitle) ? '' : ' &vellip; ' . $subtitle !!}</title>
+    <link rel="shortcut icon" href="{{ asset('assets/images/favicon.png') }}" type="image/png">
+    {{-- Load resources --}}
+    <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/all.min.css') }}">
+    @vite('resources/css/app.css')
 </head>
 <body class="bg-zinc-200">
 
-    {{-- User top bar --}}
+{{-- User top bar --}}
+<x-layouts.user_top_bar/>
 
-    {{-- Main horizontal menu --}}
-
-    {{-- Main content --}}
-    <div class="p-8">
-        {{ $slot }}
-    </div>
+{{-- Main horizontal menu --}}
+<x-layouts.main_menu/>
+{{-- Main content --}}
+<div class="p-8">
+    {{ $slot }}
+</div>
 
 
 </body>
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+<x-layouts.auth-layout subtitle="{{ empty($subtitle) ? '' : $subtitle }}">
+    <div class="main-card">
+        Lorem ipsum dollor sit amet, consectetur elit.
+    </div>
+</x-layouts.auth-layout>
