f98976a49afc813ec5ab09b8fa9c75030990268d
Marcio Pelegrini <marcio.pelegrini@gmail.com>
2026-03-06 15:26:49 -0500

    Created seeder ticket

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,29 +3,55 @@
 namespace App\Http\Controllers;
 
 use App\Models\Queue;
-use App\Models\QueueTicket;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class MainController extends Controller
 {
     public function index()
     {
-        // get list of active queues for the auth user's company
-        $queues = $this->getQueueList();
+        // get list of active queues for the authenticated user's company
+        $queues = $this->getQueuesList();
+
         $data = [
             'subtitle' => 'Home',
             'queues' => $queues
         ];
+
         dd($data);
+
         return view('home', $data);
     }
 
-    public function getQueueList()
+    private function getQueuesList()
     {
-        return Queue::where('id_company', Auth::user()->id_company)
+        $companyId = Auth::user()->id_company;
+
+        return Queue::where('id_company', $companyId)
             ->where('status', 'active')
-            ->withCount('tickets')
-            ->get()->sortBy('name');
+            ->whereNull('deleted_at')
+            ->withCount([
+                'tickets as total_tickets' => function ($query) {
+                    $query->whereNotNull('queue_ticket_status')
+                        ->whereNull('deleted_at');
+                },
+                'tickets as total_dismissed' => function ($query) {
+                    $query->where('queue_ticket_status', 'dismissed')
+                        ->whereNull('deleted_at');
+                },
+                'tickets as total_not_attended' => function ($query) {
+                    $query->where('queue_ticket_status', 'not_attended')
+                        ->whereNull('deleted_at');
+                },
+                'tickets as total_called' => function ($query) {
+                    $query->where('queue_ticket_status', 'called')
+                        ->whereNull('deleted_at');
+                },
+                'tickets as total_waiting' => function ($query) {
+                    $query->where('queue_ticket_status', 'waiting')
+                        ->whereNull('deleted_at');
+                },
+            ])
+            ->get();
     }
 }
